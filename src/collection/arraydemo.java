2c5e837f06234546f869409ca834d1a6990415c0
fourt <fourt@LAPTOP-P4DS4FMF>
2023-06-27 19:28:04 +0530

    This is first commit

---
+++ src/collection/arraydemo.java
@@ -0,0 +1,10 @@
+package collection;
+
+public class arraydemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/data.java
@@ -0,0 +1,15 @@
+
+
+public class data //class
+{
+  int x=5;//var 
+  
+	public static void main(String[] args)
+	
+	{
+		data welcome=new data();
+		System.out.println(welcome.x);
+
+	} 
+
+}
